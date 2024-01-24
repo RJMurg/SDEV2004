a01644a82d40c9556671d5e9bbb77ee4df10fe77
TW-BETA\ruan <ruan@murgatroyd.me>
2024-01-24 11:22:14 +0000

    Week 1 lab completed

--- week1/app/routes.py
+++ week1/app/routes.py
@@ -4,4 +4,5 @@ from flask import render_template
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", title="beans")
+    user = {'username': 'Rupert Murdochtroyd'}
+    return render_template('index.html', title='Lab Class Week 1', user=user)
