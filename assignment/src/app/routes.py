62d2664d64a490d2ce511ba0ef883eb144c55b24
Ruán Murgatroyd <ruan@murgatroyd.me>
2024-04-17 20:13:04 +0100

    Beginning work on the assignment

---
+++ assignment/src/app/routes.py
@@ -0,0 +1,35 @@
+from app import app
+from flask import render_template, session, redirect, request
+
+@app.before_request
+def set_session_language():
+    if 'language' not in session:
+        session['language'] = 'en'  # Set default language here
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return (render_template("index.html"))
+
+@app.route('/about')
+def about():
+    return (render_template ("about.html"))
+
+@app.route('/products')
+def products():
+        return (render_template ("products.html"))
+
+@app.route('/store')
+def store():
+        return (render_template ("store.html"))
+
+@app.route('/setlang/<lang_code>')
+def set_language(lang_code):
+#Pop the session variable to None to force an overwrite
+    session.pop('language', None)
+#Set the session variable lang to whatever code was used in the url
+    session["language"] = lang_code
+#refresh the page from which the set language request was made
+    return redirect(request.referrer)
+
